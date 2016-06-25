66d9211f29c802c10f4cb8b84d9bee2460ef3043
Luke Autry <lukeautry@gmail.com>
2016-06-25 14:28:06 -0400

    wrap generator code, return out if error

--- src/swagger/cli.ts
+++ src/swagger/cli.ts
@@ -3,21 +3,27 @@
 import {SwaggerGenerator} from './generator';
 import * as yargs from 'yargs';
 
-const argv: GeneratorArguments = yargs.argv;
+(function runGenerator() {
+    const argv: GeneratorArguments = yargs.argv;
 
-interface GeneratorArguments {
-    outFile: string;
-    mainFile: string;
-}
+    interface GeneratorArguments {
+        outFile: string;
+        mainFile: string;
+    }
 
-const outFile = argv.outFile;
-if (!outFile) {
-    process.stdout.write('Must provide -outFile argument, e.g. ./dist/swagger.json');
-}
+    const outFile = argv.outFile;
+    if (!outFile) {
+        process.stdout.write('Must provide -outFile argument, e.g. ./dist/swagger.json');
+        return;
+    }
 
-const mainFile = argv.mainFile;
-if (!mainFile) {
-    process.stdout.write('Must provide -mainFile argument, e.g. ./src/server.ts');
-}
+    const mainFile = argv.mainFile;
+    if (!mainFile) {
+        process.stdout.write('Must provide -mainFile argument, e.g. ./src/server.ts');
+        return;
+    }
 
-SwaggerGenerator.Generate(outFile, mainFile);
+    SwaggerGenerator.Generate(outFile, mainFile);
+})();
+
+
