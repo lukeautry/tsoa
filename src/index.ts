e4d52db6d7490a1cce5c27af855705f5748dafb1
Luke Autry <lukeautry@gmail.com>
2016-12-18 19:43:31 -0500

    expose tags

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Example } from './decorators/example';
 import { Inject, Request } from './decorators/inject';
 import { Post, Get, Patch, Delete, Put } from './decorators/methods';
+import { Tags } from './decorators/tags';
 import { Route } from './decorators/route';
 import { JWT } from './decorators/jwt';
 import { ValidateParam } from './routeGeneration/templateHelpers';
@@ -18,5 +19,6 @@ export {
   Route,
   ValidateParam,
   JWT,
-  JwtHolder
+  JwtHolder,
+  Tags
 }
