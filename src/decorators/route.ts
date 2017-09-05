d32c5275f753cd36b1300de5dad410732e94447a
Andrii <andrii.mozharovskyi@observepoint.com>
2017-09-05 14:59:08 +0300

    Moved Internal annotation to separate ts file

--- src/decorators/route.ts
+++ src/decorators/route.ts
@@ -8,12 +8,3 @@ export function Route(name?: string): any {
 export function Hidden(): any {
   return () => { return; };
 }
-
-/**
- * Marks controller or method to be generated in private Swagger file
- * @return {any}
- * @constructor
- */
-export function Internal(): any {
-  return () => { return; };
-}
---
+++ src/decorators/swagger.ts
@@ -0,0 +1,8 @@
+/**
+ * Marks controller or method to be generated in private Swagger file
+ * @return {any}
+ * @constructor
+ */
+export function Internal(): any {
+  return () => { return; };
+}
