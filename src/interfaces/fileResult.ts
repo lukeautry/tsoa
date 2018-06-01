a32ca42a6432b1ad85912d57157477249891c9e0
Roy Milde <roy.milde@nubix.biz>
2018-06-01 13:18:47 +0200

    improve file result creation, use sendFile instead of download for downloading files

--- src/interfaces/fileResult.ts
+++ src/interfaces/fileResult.ts
@@ -2,4 +2,14 @@ export class FileResult {
   public path?: string;
   public data?: any;
   public filename?: string;
+
+  constructor(initial: FileResult = {}) {
+    this.path = initial.path;
+    this.data = initial.data;
+    this.filename = initial.filename;
+  }
+
+  public static newInstance(initial: FileResult = {}): FileResult {
+    return new FileResult(initial);
+  }
 }
--- tests/fixtures/express/routes.ts
+++ tests/fixtures/express/routes.ts
@@ -1959,7 +1959,7 @@ export function RegisterRoutes(app: any) {
           if (data instanceof FileResult) {
             if (data.path) {
               response.status(statusCode|200);
-              data.filename? response.download(data.path, data.filename):response.download(data.path)
+              data.filename? response.sendFile(data.path, data.filename):response.sendFile(data.path);
             } else if (data.data) {
               if (data.data instanceof Readable) {
                 response.status(statusCode|200);
