b5f38c32d39a58c09eb580c0cd27993a18fbb388
James Tharpe <james@brainbytescreative.com>
2018-06-08 22:16:06 -0400

    Adds routeTo method for HATEOAS support

--- src/decorators/methods.ts
+++ src/decorators/methods.ts
@@ -1,19 +1,56 @@
+export const keys = {
+  baseRoute: '__base_route',
+  controllerClass: '__controller_class',
+  pathExpression: '__pathExpression',
+};
+
+export function routeTo(handler, args?: object) {
+  const baseRoute = handler[keys.controllerClass][keys.baseRoute];
+  let pathValue: string = handler[keys.pathExpression] || '';
+  if (args && pathValue) {
+    const argProps = Object.getOwnPropertyNames(args);
+    for (const argProp of argProps) {
+      pathValue = pathValue.replace('{' + argProp + '}', args[argProp]);
+    }
+  }
+  return (
+    '/' + baseRoute + '/' + encodeURI(pathValue)
+  );
+}
+
+// const saveRoute =
+
 export function Get(value?: string): any {
-  return () => { return; };
+  return (target: any, propertyKey: string) => {
+    target[propertyKey][keys.controllerClass] = target.constructor;
+    target[propertyKey][keys.pathExpression] = value;
+  };
 }
 
 export function Post(value?: string): any {
-  return () => { return; };
+  return (target: any, propertyKey: string) => {
+    target[propertyKey][keys.controllerClass] = target.constructor;
+    target[propertyKey][keys.pathExpression] = value;
+  };
 }
 
 export function Put(value?: string): any {
-  return () => { return; };
+  return (target: any, propertyKey: string) => {
+    target[propertyKey][keys.controllerClass] = target.constructor;
+    target[propertyKey][keys.pathExpression] = value;
+  };
 }
 
 export function Patch(value?: string): any {
-  return () => { return; };
+  return (target: any, propertyKey: string) => {
+    target[propertyKey][keys.controllerClass] = target.constructor;
+    target[propertyKey][keys.pathExpression] = value;
+  };
 }
 
 export function Delete(value?: string): any {
-  return () => { return; };
+  return (target: any, propertyKey: string) => {
+    target[propertyKey][keys.controllerClass] = target.constructor;
+    target[propertyKey][keys.pathExpression] = value;
+  };
 }
