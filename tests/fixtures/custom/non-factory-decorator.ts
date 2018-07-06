4fc4d0a52ad6b68b863e3388137d9e7782789840
Matthias Kunnen <matthias.kunnen@gmail.com>
2018-07-06 16:38:03 +0200

    Added test for simple decorator expression

---
+++ tests/fixtures/custom/non-factory-decorator.ts
@@ -0,0 +1,4 @@
+export function NonFactoryDecorator(target: any, propertyName: string): void {
+  // Decorators that are not Decorator Factories should be able to be used
+  // alongside tsoa decorators
+}
