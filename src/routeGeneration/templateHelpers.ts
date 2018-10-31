83d29f27bf3fd69af98463572d7e4c764fae39d8
Cerberuser <k.bagrov@g.nsu.ru>
2018-10-31 15:29:15 +0700

    Reverted change to vscode.json; all values are treated as nullable

--- src/routeGeneration/templateHelpers.ts
+++ src/routeGeneration/templateHelpers.ts
@@ -25,11 +25,7 @@ export function ValidateParam(property: TsoaRoute.PropertySchema, value: any, ge
       };
       return;
     } else {
-      if (property.dataType === 'any') {
-        return property.default !== undefined ? property.default : value;
-      } else {
-        return property.default;
-      }
+      return property.default !== undefined ? property.default : value;
     }
   }
 
