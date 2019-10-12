d8652da9770e94968a3c84f2093b3893acb58234
WoH <wolfgang.hobmaier@sensoring.de>
2019-10-12 02:38:41 +0200

    GenerateMetadataError Specs

---
+++ tests/unit/utilities/GenerateMetadataError.spec.ts
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import 'mocha';
+import { join } from 'path';
+import { createProgram, InterfaceDeclaration, isInterfaceDeclaration, PropertySignature } from 'typescript';
+import { GenerateMetadataError } from '../../../src/metadataGeneration/exceptions';
+
+const path = join(__dirname, '../../fixtures/program.ts');
+const program = createProgram([path], {});
+program.getTypeChecker();
+const sourceFile = program.getSourceFiles().filter(sourceFile => sourceFile.fileName === path)[0];
+
+const iface = sourceFile
+  .getChildren()[0]
+  .getChildren()
+  .find(child => isInterfaceDeclaration(child)) as InterfaceDeclaration;
+
+const propertySignature = iface.members[2] as PropertySignature;
+const type = propertySignature.type!;
+
+describe('GenerateMetadataError', () => {
+  it(`Should have a given text`, () => {
+    expect(new GenerateMetadataError('Text').message).to.eq('Text');
+  });
+
+  it(`Should give context on a failing type if provided`, () => {
+    expect(new GenerateMetadataError('Text', type).message).to.eq(`Text\nAt: ${path}:4:4.\nThis was caused by 'fail: unknown;'`);
+  });
+});
