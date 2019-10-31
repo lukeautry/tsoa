2c32a89c8f2cbcb44936a72e1872ebdeb7345fb7
Dan Goldstein <dan@hyperawesome.com>
2019-10-31 16:41:30 -0400

    fix to generate OpenAPI3 requestBody output for @BodyProps

--- src/swagger/specGenerator3.ts
+++ src/swagger/specGenerator3.ts
@@ -178,6 +178,7 @@ export class SpecGenerator3 extends SpecGenerator {
     }
 
     const bodyParams = method.parameters.filter(p => p.in === 'body');
+    const bodyPropParams = method.parameters.filter(p => p.in === 'body-prop');
 
     pathMethod.parameters = method.parameters
       .filter(p => {
@@ -191,6 +192,8 @@ export class SpecGenerator3 extends SpecGenerator {
 
     if (bodyParams.length > 0) {
       pathMethod.requestBody = this.buildRequestBody(controllerName, method, bodyParams[0]);
+    } else if (bodyPropParams.length > 0) {
+      pathMethod.requestBody = this.buildRequestBodyUsingBodyProps(controllerName, method);
     }
   }
 
@@ -236,6 +239,35 @@ export class SpecGenerator3 extends SpecGenerator {
     }
 
     return {
+      content: {
+        'application/json': { schema } as Swagger.MediaType,
+      },
+    } as Swagger.RequestBody;
+  }
+
+  private buildRequestBodyUsingBodyProps(controllerName: string, method: Tsoa.Method) {
+    const properties = {} as { [name: string]: Swagger.Schema3 };
+    const required: string[] = [];
+
+    method.parameters
+      .filter(p => p.in === 'body-prop')
+      .forEach(p => {
+        properties[p.name] = this.getSwaggerType(p.type) as Swagger.Schema3;
+        properties[p.name].default = p.default;
+        properties[p.name].description = p.description;
+
+        if (p.required) {
+          required.push(p.name);
+        }
+      });
+
+    if (!Object.keys(properties).length) {
+      return undefined;
+    }
+
+    let schema: Swagger.Schema = { type: 'object', properties: properties, required: required };
+    return {
+      required: required.length > 0,
       content: {
         'application/json': { schema } as Swagger.MediaType,
       },
