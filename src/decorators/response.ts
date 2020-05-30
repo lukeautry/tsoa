e23fcf805f9c7f1a18641936feb13c26e530253e
WoH <wolfgang.hobmaier@sensoring.de>
2020-05-30 16:22:25 +0200

    Inline Docs

--- src/decorators/response.ts
+++ src/decorators/response.ts
@@ -10,6 +10,11 @@ export function Response<T>(name: string | number, description?: string, example
   };
 }
 
+/**
+ * Inject a library-agnostic responder function that can be used to construct type-checked (usually error-) responses.
+ *
+ * The type of the responder function should be annotated `TsoaResponse<Status, Data, Headers>` in order to support OpenAPI documentation.
+ */
 export function Res(): Function {
   return () => {
     return;
