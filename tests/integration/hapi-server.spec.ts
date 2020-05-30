3073b7e5d6b36e524112a307181dbb54e89d3e41
WoH <wolfgang.hobmaier@sensoring.de>
2020-05-30 15:57:32 +0200

    Hapi Support

--- tests/integration/hapi-server.spec.ts
+++ tests/integration/hapi-server.spec.ts
@@ -1028,13 +1028,15 @@ describe('Hapi Server', () => {
       });
     });
 
-    it('Should ignore @Res', () => {
+    it('Should return on @Res', () => {
       return verifyGetRequest(
         basePath + '/GetTest/Res',
         (err, res) => {
-          expect(res.body).to.deep.equal({});
+          const model = res.body as TestModel;
+          expect(model.id).to.equal(1);
+          expect(res.get('custom-header')).to.eq('hello');
         },
-        204,
+        400,
       );
     });
   });
