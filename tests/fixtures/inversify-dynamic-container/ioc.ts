1da6ec412cbde0604f36ac33e32e17754357879a
Alex Ward <alxwrd@googlemail.com>
2020-10-02 15:48:35 +0100

    use new ioc container typings in test

--- tests/fixtures/inversify-dynamic-container/ioc.ts
+++ tests/fixtures/inversify-dynamic-container/ioc.ts
@@ -2,8 +2,9 @@ import e = require('express');
 import { Container } from 'inversify';
 import { ManagedController } from './managedController';
 import { ManagedService } from './managedService';
+import { IocContainerFactory, IocContainer } from '@tsoa/runtime';
 
-const iocContainer = function (request: e.Request): Container {
+const iocContainer: IocContainerFactory = function (request: e.Request): IocContainer {
   const container = new Container();
   container.bind<string>(Symbol.for('requestPath')).toConstantValue(request.path);
   container.bind<ManagedService>(ManagedService).to(ManagedService);
