f797ef37855365a33eba31a21ae9b0ebf969b5cb
WoH <wolfgang.hobmaier@sensoring.de>
2021-01-09 18:31:50 +0100

    fix(headers): Allow string indexed access

--- packages/runtime/src/utils/isHeaderType.ts
+++ packages/runtime/src/utils/isHeaderType.ts
@@ -1,8 +1,8 @@
 /**
  * Checks for a supported header type
- * key types can be `never` to support `{}` or `number` to support
+ * key types can be `number` to support string indexed access types
  */
-export type IsValidHeader<Header> = keyof Header extends string
+export type IsValidHeader<Header> = keyof Header extends string | number
   ? Header[keyof Header] extends string | string[] | undefined
     ? {}
     : 'Header values must be string or string[]'
