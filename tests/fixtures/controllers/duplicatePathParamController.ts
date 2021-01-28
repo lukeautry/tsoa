4903efe66037682da4862f8756caee50f6c75ed0
simon <simon.tretter@hokify.com>
2021-01-28 12:29:27 +0100

    refactor: further simplify and improve path collision detection

--- tests/fixtures/controllers/duplicatePathParamController.ts
+++ tests/fixtures/controllers/duplicatePathParamController.ts
@@ -38,7 +38,7 @@ export class DuplicatePathParamTestController {
     return id;
   }
 
-  // This method should not report error cause its prefix route is different.
+  // This method should not report a warning cause its prefix route is different.
   @Delete('Delete2/{id}') public deletePathParamTest2(@Path() id: string) {
     return id;
   }
@@ -47,12 +47,32 @@ export class DuplicatePathParamTestController {
     return identifier;
   }
 
-  // These two Head method should raise only warn but still work.
+  // These two Head method should not raise a warning
   @Head('Head/{id}') public headPathParamTest(@Path() id) {
     return id;
   }
 
   @Head('Head/{id}/{id2}') public headPathParamTest2(@Path() id, @Path() id2) {
     return { id, id2 };
+  }
+
+  // These two Head method should raise a warning,
+  // because in this order "id" could be "something-like-this" and would match
+  @Head('Head-PartialCollision/{id}') public headPartialCollisionPathParamTest(@Path() id) {
+    return id;
+  }
+
+  @Head('Head-PartialCollision/{id}-{id2}') public headPartialCollisionPathParamTest2(@Path() id, @Path() id2) {
+    return { id, id2 };
+  }
+
+  // These two Head method should not raise a warning,
+  // as the order is fine
+  @Head('Head-NoPartialCollision/{id}-{id2}') public headNoPartialCollisionPathParamTest2(@Path() id, @Path() id2) {
+    return { id, id2 };
+  }
+
+  @Head('Head-NoPartialCollision/{id}') public headNoPartialCollisionPathParamTest(@Path() id) {
+    return id;
   }
 }
