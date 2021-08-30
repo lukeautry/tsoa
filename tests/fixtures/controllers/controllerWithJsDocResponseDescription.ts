56b2ce06a9b6e3749787117ae3084b2830fb12a8
pauliyobo <pauliyobo@gmail.com>
2021-08-30 12:08:45 +0200

    added new fixture for custom response descriptions

---
+++ tests/fixtures/controllers/controllerWithJsDocResponseDescription.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Route, SuccessResponse } from '@tsoa/runtime';
+import { TestModel } from 'fixtures/duplicateTestModel';
+import { ModelService } from 'fixtures/services/modelService';
+
+@Route('Controller')
+export class CustomResponseDescController extends Controller {
+  @Get('descriptionWithSuccessResponse')
+  @SuccessResponse(200, 'SuccessResponse description')
+  public async descriptionWithSuccessResponse(): Promise<TestModel> {
+    return new ModelService().getModel();
+  }
+
+  @Get('descriptionWithJsDocAnnotation')
+  /**
+   * @returns custom description with jsdoc annotation
+   */
+  public async descriptionWithJsDocAnnotation(): Promise<TestModel> {
+    return new ModelService().getModel();
+  }
+
+  @Get('successResponseAndJsDocAnnotation')
+  /**
+   * @returns custom description from jsdoc annotation
+   */
+  @SuccessResponse(200, 'Success Response description')
+  public async successResponseAndJsDocAnnotation(): Promise<TestModel> {
+    return new ModelService().getModel();
+  }
+}
