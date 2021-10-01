523a9abff2b4fbdff8d9f50e8fcbdae5d9dad329
DjakaTechnology <djakajaya89@gmail.com>
2021-10-01 19:21:37 +0700

    Added negative test

---
+++ tests/fixtures/controllers/invalidExtensionModelController.ts
@@ -0,0 +1,19 @@
+import { Get, Route } from '@tsoa/runtime';
+
+interface InvalidExtension {
+  /**
+   * @extension {"key-1": "value-1"}
+   */
+  invalidExtension: number;
+}
+
+@Route('BadExtensionTest')
+export class InvalidExtensionModelController {
+  // Vendor extensions must start with 'x-'
+  @Get('badExtension')
+  public async badExtensionMethod(): Promise<InvalidExtension> {
+    return {
+      invalidExtension: 1,
+    };
+  }
+}
