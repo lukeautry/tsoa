2556d860c4662b9635195dfdb3d356d8e0b9efa9
WoH <wolfgang.hobmaier@sensoring.de>
2022-10-01 10:02:38 +0200

    fix: Code Style

--- tests/fixtures/controllers/controllerWithCommonResponses.ts
+++ tests/fixtures/controllers/controllerWithCommonResponses.ts
@@ -1,16 +1,16 @@
 import { Controller, Get, Response, Post, Route } from '@tsoa/runtime';
 import { ModelService } from '../services/modelService';
-import { ErrorResponseModel, TestModel as ResponseModel } from '../testModel';
+import { ErrorResponseModel, TestModel } from '../testModel';
 
 @Route('Controller')
 @Response<ErrorResponseModel>('401', 'Unauthorized')
 export class HiddenMethodController extends Controller {
   @Get('protectedGetMethod')
-  public async protectedGetMethod(): Promise<ResponseModel> {
+  public async protectedGetMethod(): Promise<TestModel> {
     return Promise.resolve(new ModelService().getModel());
   }
   @Post('protectedPostMethod')
-  public async protectedPostMethod(): Promise<ResponseModel> {
+  public async protectedPostMethod(): Promise<TestModel> {
     return Promise.resolve(new ModelService().getModel());
   }
 }
--- tests/fixtures/controllers/methodController.ts
+++ tests/fixtures/controllers/methodController.ts
@@ -1,6 +1,6 @@
 import { Controller, Extension, Options, Delete, Get, Patch, Post, Put, Response, Route, Security, SuccessResponse, Tags, Example } from '@tsoa/runtime';
 import { ModelService } from '../services/modelService';
-import { ErrorResponseModel, TestModel as ResponseModel } from '../testModel';
+import { ErrorResponseModel, TestModel, TestModel as RenamedModel } from '../testModel';
 
 const TEST_OBJECT_CONST = {
   unAuthCode: '401',
@@ -30,32 +30,32 @@ const TEST_SEC = {
 @Route('MethodTest')
 export class MethodController extends Controller {
   @Options('Options')
-  public async optionsMethod(): Promise<ResponseModel> {
+  public async optionsMethod(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
   @Get('Get')
-  public async getMethod(): Promise<ResponseModel> {
+  public async getMethod(): Promise<RenamedModel> {
     return new ModelService().getModel();
   }
 
   @Post('Post')
-  public async postMethod(): Promise<ResponseModel> {
+  public async postMethod(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
   @Patch('Patch')
-  public async patchMethod(): Promise<ResponseModel> {
+  public async patchMethod(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
   @Put('Put')
-  public async putMethod(): Promise<ResponseModel> {
+  public async putMethod(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
   @Delete('Delete')
-  public async deleteMethod(): Promise<ResponseModel> {
+  public async deleteMethod(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
@@ -63,13 +63,13 @@ export class MethodController extends Controller {
    * method description
    */
   @Get('Description')
-  public async description(): Promise<ResponseModel> {
+  public async description(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
   @Tags('Tag1', 'Tag2', 'Tag3')
   @Get('Tags')
-  public async tags(): Promise<ResponseModel> {
+  public async tags(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
@@ -77,7 +77,7 @@ export class MethodController extends Controller {
   @Response<ErrorResponseModel>('401', 'Unauthorized')
   @Response<ErrorResponseModel>('default', 'Unexpected error', { status: 500, message: 'Something went wrong!' })
   @Get('MultiResponse')
-  public async multiResponse(): Promise<ResponseModel> {
+  public async multiResponse(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
@@ -89,7 +89,7 @@ export class MethodController extends Controller {
   @Response<ErrorResponseModel>(TEST_ENUM_CODES.NOT_FOUND, TEST_ENUM.NOT_FOUND)
   @SuccessResponse(TEST_ENUM_CODES.SUCCESS, TEST_OBJECT_CONST.success)
   @Get('DecoratorVariousValues')
-  public async decoratorVariousValues(): Promise<ResponseModel> {
+  public async decoratorVariousValues(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
@@ -102,20 +102,20 @@ export class MethodController extends Controller {
 
   @Security('api_key')
   @Get('ApiSecurity')
-  public async apiSecurity(): Promise<ResponseModel> {
+  public async apiSecurity(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
   @Security('tsoa_auth', ['write:pets', 'read:pets'])
   @Get('OauthSecurity')
-  public async oauthSecurity(): Promise<ResponseModel> {
+  public async oauthSecurity(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
   @Security('tsoa_auth', ['write:pets', 'read:pets'])
   @Security('api_key')
   @Get('OauthOrApiKeySecurity')
-  public async oauthOrAPIkeySecurity(): Promise<ResponseModel> {
+  public async oauthOrAPIkeySecurity(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
@@ -124,7 +124,7 @@ export class MethodController extends Controller {
     tsoa_auth: ['write:pets', 'read:pets'],
   })
   @Get('OauthAndApiKeySecurity')
-  public async oauthAndAPIkeySecurity(): Promise<ResponseModel> {
+  public async oauthAndAPIkeySecurity(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
@@ -132,7 +132,7 @@ export class MethodController extends Controller {
    * @deprecated
    */
   @Get('DeprecatedMethod')
-  public async deprecatedMethod(): Promise<ResponseModel> {
+  public async deprecatedMethod(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
@@ -140,7 +140,7 @@ export class MethodController extends Controller {
    * @summary simple summary
    */
   @Get('SummaryMethod')
-  public async summaryMethod(): Promise<ResponseModel> {
+  public async summaryMethod(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 
@@ -165,7 +165,7 @@ export class MethodController extends Controller {
   @Extension('x-attKey7', { test: ['testVal', 123, true, null] })
   @Extension('x-attKey8', { test: { testArray: ['testVal1', true, null, ['testVal2', 'testVal3', 123, true, null]] } })
   @Get('Extension')
-  public async extension(): Promise<ResponseModel> {
+  public async extension(): Promise<TestModel> {
     return new ModelService().getModel();
   }
 }
