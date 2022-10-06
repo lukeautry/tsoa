11dff40d9c8d698157c8272a62408379146ce411
Clooooode <jackey8616@gmail.com>
2022-10-06 23:36:51 +0800

    test: Remove try-catch

--- tests/integration/express-server.spec.ts
+++ tests/integration/express-server.spec.ts
@@ -1400,12 +1400,10 @@ describe('Express Server', () => {
 
     it.only('cannot post a file with wrong attribute name', async () => {
       const formData = { wrongAttributeName: '@../package.json' };
-      try {
-        await verifyFileUploadRequest(basePath + '/PostTest/File', formData);
-      } catch (e: any) {
-        expect(e.response.status).to.equal(500);
-        expect(e.response.text).to.equal('{"message":"Unexpected field","name":"MulterError","status":500}');
-      }
+      verifyFileUploadRequest(basePath + '/PostTest/File', formData, (_err, res) => {
+        expect(res.status).to.equal(500);
+        expect(res.text).to.equal('{"message":"Unexpected field","name":"MulterError","status":500}');
+      });
     });
 
     it('can post multiple files with other form fields', () => {
