305b8c4e6d1944f0e0f0f06ce1b2d9bb2351896d
Clooooode <jackey8616@gmail.com>
2023-01-31 00:11:21 +0800

    fix: controller path @ generator

--- tests/unit/swagger/schemaDetails.spec.ts
+++ tests/unit/swagger/schemaDetails.spec.ts
@@ -493,7 +493,7 @@ describe('Schema details generation', () => {
         });
 
         it('Should not generate models with hidden controller referenced.', () => {
-          const metadata = new MetadataGenerator('./fixtures/controllers/CommonResponseHiddenModelController.ts').Generate();
+          const metadata = new MetadataGenerator('./fixtures/controllers/commonResponseHiddenModelController.ts').Generate();
           const responseSpec = new SpecGenerator2(metadata, getDefaultExtendedOptions()).GetSpec();
           expect(responseSpec.definitions).to.be.deep.eq({});
         });
