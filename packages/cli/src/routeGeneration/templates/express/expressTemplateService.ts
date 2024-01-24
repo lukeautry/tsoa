79a057eaed43737153410f5855c78871b8a2408a
Clooooode <jackey8616@gmail.com>
2024-01-24 09:09:40 +0800

    chore: lint

--- packages/cli/src/routeGeneration/templates/express/expressTemplateService.ts
+++ packages/cli/src/routeGeneration/templates/express/expressTemplateService.ts
@@ -1,5 +1,5 @@
 import { Request as ExRequest, Response as ExResponse } from 'express';
-import { FieldErrors, HttpStatusCodeLiteral, TsoaResponse, ValidateError, ValidationService } from "@tsoa/runtime";
+import { FieldErrors, HttpStatusCodeLiteral, TsoaResponse, ValidateError, ValidationService } from '@tsoa/runtime';
 
 import { TemplateService, isController } from '../templateService';
 
@@ -25,73 +25,74 @@ export class ExpressTemplateService implements TemplateService<ExRequest, ExResp
 
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
 
-        this.returnHandler(response, headers, statusCode, data)
+        this.returnHandler(response, headers, statusCode, data);
       })
       .catch((error: any) => next(error));
   }
 
   returnHandler(response: any, headers: any = {}, statusCode?: number, data?: any) {
     if (response.headersSent) {
-        return;
+      return;
     }
     Object.keys(headers).forEach((name: string) => {
-        response.set(name, headers[name]);
+      response.set(name, headers[name]);
     });
     if (data && typeof data.pipe === 'function' && data.readable && typeof data._read === 'function') {
-        response.status(statusCode || 200)
-        data.pipe(response);
+      response.status(statusCode || 200);
+      data.pipe(response);
     } else if (data !== null && data !== undefined) {
-        response.status(statusCode || 200).json(data);
+      response.status(statusCode || 200).json(data);
     } else {
-        response.status(statusCode || 204).end();
+      response.status(statusCode || 204).end();
     }
   }
 
-  responder(response: any): TsoaResponse<HttpStatusCodeLiteral, unknown>  {
+  responder(response: any): TsoaResponse<HttpStatusCodeLiteral, unknown> {
     return (status, data, headers) => {
-        this.returnHandler(response, headers, status, data);
+      this.returnHandler(response, headers, status, data);
     };
   }
 
   getValidatedArgs(args: any, request: ExRequest, response: ExResponse): any[] {
-    const fieldErrors: FieldErrors  = {};
-    const values = Object.keys(args).map((key) => {
-        const name = args[key].name;
-        switch (args[key].in) {
-            case 'request':
-                return request;
-            case 'request-prop':
-                return this.validationService.ValidateParam(args[key], (request as any)[name], name, fieldErrors, undefined, this.minimalSwaggerConfig);
-            case 'query':
-                return this.validationService.ValidateParam(args[key], request.query[name], name, fieldErrors, undefined, this.minimalSwaggerConfig);
-            case 'queries':
-                return this.validationService.ValidateParam(args[key], request.query, name, fieldErrors, undefined, this.minimalSwaggerConfig);
-            case 'path':
-                return this.validationService.ValidateParam(args[key], request.params[name], name, fieldErrors, undefined, this.minimalSwaggerConfig);
-            case 'header':
-                return this.validationService.ValidateParam(args[key], request.header(name), name, fieldErrors, undefined, this.minimalSwaggerConfig);
-            case 'body':
-                return this.validationService.ValidateParam(args[key], request.body, name, fieldErrors, undefined, this.minimalSwaggerConfig);
-            case 'body-prop':
-                return this.validationService.ValidateParam(args[key], request.body[name], name, fieldErrors, 'body.', this.minimalSwaggerConfig);
-            case 'formData':
-              const files = Object.keys(args).filter((argKey) => args[argKey].dataType === 'file');
-                if (files.length > 0) {
-                  const requestFiles = request.files as {[fileName: string]: Express.Multer.File[]};
-                  const fileArgs = this.validationService.ValidateParam(args[key], requestFiles[name], name, fieldErrors, undefined, this.minimalSwaggerConfig);
-                  return fileArgs.length === 1 ? fileArgs[0] : fileArgs;
-                } else if (args[key].dataType === 'array' && args[key].array.dataType === 'file') {
-                    return this.validationService.ValidateParam(args[key], request.files, name, fieldErrors, undefined, this.minimalSwaggerConfig);
-                } else {
-                    return this.validationService.ValidateParam(args[key], request.body[name], name, fieldErrors, undefined, this.minimalSwaggerConfig);
-                }
-            case 'res':
-                return this.responder(response);
+    const fieldErrors: FieldErrors = {};
+    const values = Object.keys(args).map(key => {
+      const name = args[key].name;
+      switch (args[key].in) {
+        case 'request':
+          return request;
+        case 'request-prop':
+          return this.validationService.ValidateParam(args[key], (request as any)[name], name, fieldErrors, undefined, this.minimalSwaggerConfig);
+        case 'query':
+          return this.validationService.ValidateParam(args[key], request.query[name], name, fieldErrors, undefined, this.minimalSwaggerConfig);
+        case 'queries':
+          return this.validationService.ValidateParam(args[key], request.query, name, fieldErrors, undefined, this.minimalSwaggerConfig);
+        case 'path':
+          return this.validationService.ValidateParam(args[key], request.params[name], name, fieldErrors, undefined, this.minimalSwaggerConfig);
+        case 'header':
+          return this.validationService.ValidateParam(args[key], request.header(name), name, fieldErrors, undefined, this.minimalSwaggerConfig);
+        case 'body':
+          return this.validationService.ValidateParam(args[key], request.body, name, fieldErrors, undefined, this.minimalSwaggerConfig);
+        case 'body-prop':
+          return this.validationService.ValidateParam(args[key], request.body[name], name, fieldErrors, 'body.', this.minimalSwaggerConfig);
+        case 'formData': {
+          const formFiles = Object.keys(args).filter(argKey => args[argKey].dataType === 'file');
+          if (formFiles.length > 0) {
+            const requestFiles = request.files as { [fileName: string]: Express.Multer.File[] };
+            const fileArgs = this.validationService.ValidateParam(args[key], requestFiles[name], name, fieldErrors, undefined, this.minimalSwaggerConfig);
+            return fileArgs.length === 1 ? fileArgs[0] : fileArgs;
+          } else if (args[key].dataType === 'array' && args[key].array.dataType === 'file') {
+            return this.validationService.ValidateParam(args[key], request.files, name, fieldErrors, undefined, this.minimalSwaggerConfig);
+          } else {
+            return this.validationService.ValidateParam(args[key], request.body[name], name, fieldErrors, undefined, this.minimalSwaggerConfig);
+          }
         }
+        case 'res':
+          return this.responder(response);
+      }
     });
 
     if (Object.keys(fieldErrors).length > 0) {
-        throw new ValidateError(fieldErrors, '');
+      throw new ValidateError(fieldErrors, '');
     }
     return values;
   }
--- packages/cli/src/routeGeneration/templates/koa/koaTemplateService.ts
+++ packages/cli/src/routeGeneration/templates/koa/koaTemplateService.ts
@@ -1,6 +1,6 @@
 import type { Context } from 'koa';
-import { TsoaResponse, HttpStatusCodeLiteral, FieldErrors, ValidationService, ValidateError } from "@tsoa/runtime";
-import { TemplateService, isController } from "../templateService";
+import { TsoaResponse, HttpStatusCodeLiteral, FieldErrors, ValidationService, ValidateError } from '@tsoa/runtime';
+import { TemplateService, isController } from '../templateService';
 
 export class KoaTemplateService implements TemplateService<any, Context> {
   private readonly validationService: ValidationService;
@@ -19,8 +19,8 @@ export class KoaTemplateService implements TemplateService<any, Context> {
         let headers;
 
         if (isController(controllerObj)) {
-            headers = controllerObj.getHeaders();
-            statusCode = controllerObj.getStatus() || statusCode;
+          headers = controllerObj.getHeaders();
+          statusCode = controllerObj.getStatus() || statusCode;
         }
         return this.returnHandler(context, headers, statusCode, data, next);
       })
@@ -33,14 +33,14 @@ export class KoaTemplateService implements TemplateService<any, Context> {
   returnHandler(context: Context, headers: any, statusCode?: number | undefined, data?: any, next?: any) {
     if (!context.headerSent && !(context.response as any).__tsoaResponded) {
       if (data !== null && data !== undefined) {
-          context.body = data;
-          context.status = 200;
+        context.body = data;
+        context.status = 200;
       } else {
-          context.status = 204;
+        context.status = 204;
       }
 
       if (statusCode) {
-          context.status = statusCode;
+        context.status = statusCode;
       }
 
       context.set(headers);
@@ -49,51 +49,52 @@ export class KoaTemplateService implements TemplateService<any, Context> {
     }
   }
 
-  responder(context: any, next?: any): TsoaResponse<HttpStatusCodeLiteral, unknown>  {
+  responder(context: any, next?: any): TsoaResponse<HttpStatusCodeLiteral, unknown> {
     return (status, data, headers) => {
-       this.returnHandler(context, headers, status, data, next);
+      this.returnHandler(context, headers, status, data, next);
     };
   }
 
   getValidatedArgs(args: any, request: any, context: Context, next: () => any): any[] {
     const errorFields: FieldErrors = {};
     const values = Object.keys(args).map(key => {
-        const name = args[key].name;
-        switch (args[key].in) {
+      const name = args[key].name;
+      switch (args[key].in) {
         case 'request':
             return context.request;
         case 'request-prop':
           return this.validationService.ValidateParam(args[key], (context.request as any)[name], name, errorFields, undefined, this.minimalSwaggerConfig);
         case 'query':
-            return this.validationService.ValidateParam(args[key], context.request.query[name], name, errorFields, undefined, this.minimalSwaggerConfig);
+          return this.validationService.ValidateParam(args[key], context.request.query[name], name, errorFields, undefined, this.minimalSwaggerConfig);
         case 'queries':
-            return this.validationService.ValidateParam(args[key], context.request.query, name, errorFields, undefined, this.minimalSwaggerConfig);
+          return this.validationService.ValidateParam(args[key], context.request.query, name, errorFields, undefined, this.minimalSwaggerConfig);
         case 'path':
-            return this.validationService.ValidateParam(args[key], context.params[name], name, errorFields, undefined, this.minimalSwaggerConfig);
+          return this.validationService.ValidateParam(args[key], context.params[name], name, errorFields, undefined, this.minimalSwaggerConfig);
         case 'header':
-            return this.validationService.ValidateParam(args[key], context.request.headers[name], name, errorFields, undefined, this.minimalSwaggerConfig);
+          return this.validationService.ValidateParam(args[key], context.request.headers[name], name, errorFields, undefined, this.minimalSwaggerConfig);
         case 'body':
-            return this.validationService.ValidateParam(args[key], (context.request as any).body, name, errorFields, undefined, this.minimalSwaggerConfig);
+          return this.validationService.ValidateParam(args[key], (context.request as any).body, name, errorFields, undefined, this.minimalSwaggerConfig);
         case 'body-prop':
-            return this.validationService.ValidateParam(args[key], (context.request as any).body[name], name, errorFields, 'body.', this.minimalSwaggerConfig);
-        case 'formData':
-          const files = Object.keys(args).filter((argKey) => args[argKey].dataType === 'file');
+          return this.validationService.ValidateParam(args[key], (context.request as any).body[name], name, errorFields, 'body.', this.minimalSwaggerConfig);
+        case 'formData': {
+          const files = Object.keys(args).filter(argKey => args[argKey].dataType === 'file');
           const contextRequest = context.request as any;
-            if (files.length > 0) {
-              const fileArgs = this.validationService.ValidateParam(args[key], contextRequest.files[name], name, errorFields, undefined, this.minimalSwaggerConfig);
-              return fileArgs.length === 1 ? fileArgs[0] : fileArgs;
-            } else if (args[key].dataType === 'array' && args[key].array.dataType === 'file') {
-              return this.validationService.ValidateParam(args[key], contextRequest.files, name, errorFields, undefined, this.minimalSwaggerConfig);
-            } else {
-              return this.validationService.ValidateParam(args[key], contextRequest.body[name], name, errorFields, undefined, this.minimalSwaggerConfig);
-            }
-        case 'res':
-            return this.responder(context, next);
+          if (files.length > 0) {
+            const fileArgs = this.validationService.ValidateParam(args[key], contextRequest.files[name], name, errorFields, undefined, this.minimalSwaggerConfig);
+            return fileArgs.length === 1 ? fileArgs[0] : fileArgs;
+          } else if (args[key].dataType === 'array' && args[key].array.dataType === 'file') {
+            return this.validationService.ValidateParam(args[key], contextRequest.files, name, errorFields, undefined, this.minimalSwaggerConfig);
+          } else {
+            return this.validationService.ValidateParam(args[key], contextRequest.body[name], name, errorFields, undefined, this.minimalSwaggerConfig);
+          }
         }
+        case 'res':
+          return this.responder(context, next);
+      }
     });
     if (Object.keys(errorFields).length > 0) {
-        throw new ValidateError(errorFields, '');
+      throw new ValidateError(errorFields, '');
     }
     return values;
-}
+  }
 }
