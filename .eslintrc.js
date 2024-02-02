b52f832192b4981688f275ad8a69d5aa9124d3fd
Clooooode <jackey8616@gmail.com>
2024-02-02 09:14:56 +0800

    fix: remove @typescript-eslint/restrict-template-expressions @ test

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,7 +66,6 @@ module.exports = {
         '@typescript-eslint/require-await': 'off',
         '@typescript-eslint/no-unused-vars': 'off',
         '@typescript-eslint/no-floating-promises': 'off',
-        '@typescript-eslint/restrict-template-expressions': 'warn',
         // Crashes also fail the test
         'no-unsafe-optional-chaining': 'off',
       },
