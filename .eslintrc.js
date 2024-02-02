3ba290d664de7d254d8d07020834811e5a721f82
Clooooode <jackey8616@gmail.com>
2024-02-02 09:02:09 +0800

    fix: remove @typescript-eslint/explicit-module-boundary-types

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,6 @@ module.exports = {
       },
     ],
     '@typescript-eslint/ban-types': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/naming-convention': [
       'error',
       {
