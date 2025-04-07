0468cc38634a5c192e31fdabe014240448b0d381
woh <wolfgang.hobmaier@and-solution.com>
2025-04-07 15:10:25 +0200

    chore: Inline source syntax highlighting

--- tests/unit/swagger/definitionsGeneration/typeResolver.spec.ts
+++ tests/unit/swagger/definitionsGeneration/typeResolver.spec.ts
@@ -20,7 +20,7 @@ const defaultProperties = {
   validators: {},
 };
 
-function createProgramFromSource(source: string, fileName: string = 'test.ts') {
+function createProgramFromSource(source: string, fileName = 'test.ts') {
   const sourceFile = ts.createSourceFile(fileName, source, ts.ScriptTarget.Latest);
 
   const defaultCompilerHost = ts.createCompilerHost({});
@@ -40,8 +40,10 @@ function createProgramFromSource(source: string, fileName: string = 'test.ts') {
 
   const program = ts.createProgram([sourceFile.fileName], {}, compilerHost);
   const metadataGenerator = new MetadataGenerator(fileName);
-  (metadataGenerator as any).program = program;
-  (metadataGenerator as any).typeChecker = program.getTypeChecker();
+  // @ts-expect-error: 'program' is a private and read-only property of MetadataGenerator
+  metadataGenerator.program = program;
+  // @ts-expect-error: 'program' is a private and read-only property of MetadataGenerator
+  metadataGenerator.typeChecker = program.getTypeChecker();
 
   const typeAliasDeclaration = sourceFile.statements.filter(ts.isTypeAliasDeclaration).pop();
   if (!typeAliasDeclaration) {
@@ -56,7 +58,10 @@ function createProgramFromSource(source: string, fileName: string = 'test.ts') {
 
 describe('TypeResolver.resolve', () => {
   it('should resolve an object type', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type ObjectType = { a: string; b: number; };');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type ObjectType = { a: string; b: number; };
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -80,9 +85,14 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve conditional types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type ConditionalType<T> = T extends string ? string : number; type Concrete = ConditionalType<boolean>;');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type ConditionalType<T> = T extends string ? string : number; 
+      type Concrete = ConditionalType<boolean>;
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
+
     expect(result).to.deep.equal({
       refName: 'ConditionalType_boolean_',
       dataType: 'refAlias',
@@ -92,15 +102,21 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve indexed access types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type IndexedAccessType = { a: string }["a"];');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type IndexedAccessType = { a: string }["a"];
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({ dataType: 'string' });
   });
 
   it('should resolve inferred types', () => {
-    const source = 'type InferredType<T> = T extends infer U ? U : never;\n' + 'type Concrete = InferredType<string>;';
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type InferredType<T> = T extends infer U ? U : never;
+      type Concrete = InferredType<string>;
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -112,11 +128,11 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve typeof with literal', () => {
-    const source = `
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
       const hello = "hello"
       type InferredType = typeof hello;
-    `;
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -126,11 +142,11 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve typeof with object', () => {
-    const source = `
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
       const concrete = {a: "a", b: 1};
       type InferredType = typeof concrete;
-    `;
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -153,7 +169,10 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve intersection types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type IntersectionType = { a: string } & { b: number };');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type IntersectionType = { a: string } & { b: number };
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -188,22 +207,31 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve intrinsic types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type IntrinsicType = string;');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type IntrinsicType = string;
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({ dataType: 'string' });
   });
 
   it('should resolve literal types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type LiteralType = "literal";');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type LiteralType = "literal";
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({ dataType: 'enum', enums: ['literal'] });
   });
 
   it('should resolve mapped types', () => {
-    const source = 'type MappedType<T> = { [P in keyof T]: T[P] }; \n' + 'type Concrete = MappedType<{ a: string; b: number }>;';
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type MappedType<T> = { [P in keyof T]: T[P] }; 
+      type Concrete = MappedType<{ a: string; b: number }>;
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -260,7 +288,10 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve optional types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type OptionalType = { a?: string };');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type OptionalType = { a?: string };
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -278,11 +309,11 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve predicate types', () => {
-    const source = `
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
       type PredicateType = (x: any) => x is string;
       type Concrete = ReturnType<PredicateType>;
-    `;
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -295,14 +326,20 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve keyof query types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type QueryType = keyof { a: string };');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type QueryType = keyof { a: string };
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({ dataType: 'enum', enums: ['a'] });
   });
 
   it('should resolve reference types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type ReferenceType = { a: string };');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type ReferenceType = { a: string };
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -321,15 +358,15 @@ describe('TypeResolver.resolve', () => {
 
   // works with nested types, but not with interfaces
   it.skip('should resolve nested interface types', () => {
-    const source = `
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
       interface Address { line1: string; line2?: string; postalCode: string; };
       type Person = { 
         first: string;
         last: string;
         address: Address;
       };
-    `;
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -347,8 +384,11 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve rest types', () => {
-    const source = 'type Sum = (...numbers: number[]) => number;\n' + 'type RestType = Parameters<Sum>;';
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type Sum = (...numbers: number[]) => number;
+      type RestType = Parameters<Sum>;
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -367,39 +407,51 @@ describe('TypeResolver.resolve', () => {
 
   // Only template literal types with literal string unions are supported
   it('should resolve template literal types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type TemplateLiteralType = `prefix_${"a" | "b"}`;');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type TemplateLiteralType = \`prefix_\${"a" | "b"}\`;
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({ dataType: 'enum', enums: ['prefix_a', 'prefix_b'] });
   });
 
   it('should resolve type operator types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type TypeOperatorType = keyof { a: string };');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type TypeOperatorType = keyof { a: string };
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({ dataType: 'enum', enums: ['a'] });
   });
 
   it('should resolve union types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type UnionType = string | number;');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type UnionType = string | number;
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({ dataType: 'union', types: [{ dataType: 'string' }, { dataType: 'double' }] });
   });
 
   it('should resolve unknown types', () => {
-    const { metadataGenerator, typeNode } = createProgramFromSource('type UnknownType = unknown;');
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
+      type UnknownType = unknown;
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({ dataType: 'any' });
   });
 
   it('should resolve simple generic types', () => {
-    const source = `
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
       type Wrapper<T> = { data: T };
       type Concrete = Wrapper<string>;
-    `;
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -417,11 +469,11 @@ describe('TypeResolver.resolve', () => {
   // One of the issues that blocks zod's z.infer from working as expected
   // see https://github.com/lukeautry/tsoa/issues/1256#issuecomment-2649340573
   it.skip('should resolve indexed generic types', () => {
-    const source = `
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
       type Type<Output> = { _type: Output };
       type ValueType = Type<string>["_type"];
-    `;
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -436,12 +488,12 @@ describe('TypeResolver.resolve', () => {
 
   // Note: requires installing zod as a dependency as well
   it.skip('should resolve Zod inferred types', () => {
-    const source = `
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
       import { z } from 'zod';
       const schema = z.object({ a: z.string(), b: z.number() });
       type InferredType = z.infer<typeof schema>;
-    `;
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -455,12 +507,12 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve references to inferred types with intrinsic', () => {
-    const source = `
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
       const value: string = "a";
       type Infer<T> = T;
       type ValueType = Infer<typeof value>;
-    `;
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -472,12 +524,12 @@ describe('TypeResolver.resolve', () => {
   });
 
   it('should resolve references to inferred types with literal', () => {
-    const source = `
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
       const value = "a";
       type Infer<T> = T;
       type ValueType = Infer<typeof value>;
-    `;
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
@@ -488,12 +540,12 @@ describe('TypeResolver.resolve', () => {
     });
   });
   it('should resolve references to inferred types with object', () => {
-    const source = `
+    const { metadataGenerator, typeNode } = createProgramFromSource(/*ts*/ `
       const value = { a: "a", b: 1};
       type Infer<T> = T;
       type ValueType = Infer<typeof value>;
-    `;
-    const { metadataGenerator, typeNode } = createProgramFromSource(source);
+    `);
+
     const resolver = new TypeResolver(typeNode, metadataGenerator);
     const result = resolver.resolve();
     expect(result).to.deep.equal({
